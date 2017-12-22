593503ab7c1e0657bd0a562a6d8395a4fc2a8c68
Or Priesender <orpris12@gmail.com>
2017-12-22 14:01:59 +0200

    Initial project upload

---
+++ app/src/main/java/com/example/orpriesender/karaoke/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.orpriesender.karaoke;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class MainActivity extends Activity {
+
+    private Button record;
+    private Button stop;
+    private MediaRecorder recorder;
+    private String outputFile;
+    private TextView message;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.onCreate(null);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        record = (Button) findViewById(R.id.record);
+        stop = (Button) findViewById(R.id.stop);
+        message = (TextView) findViewById(R.id.message);
+
+        stop.setEnabled(false);
+
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.3gp";
+
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        recorder.setOutputFile(outputFile);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    recorder.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                recorder.start();
+                record.setEnabled(false);
+                stop.setEnabled(true);
+                message.setText("Started Recording");
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recorder.stop();
+                recorder.release();
+                recorder = null;
+                stop.setEnabled(false);
+                record.setEnabled(true);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(MainActivity.this,ResultActivity.class);
+                intent.putExtra("outputFile",outputFile);
+                //change to real result from server
+                intent.putExtra("result",100);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/orpriesender/karaoke/ResultActivity.java
@@ -0,0 +1,79 @@
+package com.example.orpriesender.karaoke;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+/**
+ * Created by Or Priesender on 11-Dec-17.
+ */
+
+public class ResultActivity extends Activity {
+    Integer result;
+    String outputFile;
+    TextView result_number;
+    MediaPlayer player;
+    ImageButton play;
+    Button back;
+    private boolean isPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        outputFile = intent.getStringExtra("outputFile");
+        result = intent.getIntExtra("result", 0);
+        result_number = findViewById(R.id.result_number);
+        result_number.setText(String.valueOf(result));
+        play = findViewById(R.id.play);
+        play.setEnabled(true);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    try {
+                        player = new MediaPlayer();
+                        player.setDataSource(outputFile);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    player.prepare();
+                    player.start();
+                    player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            play.setEnabled(true);
+                        }
+                    });
+                    play.setEnabled(false);
+                    Log.d("tag", "CLICKED !");
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        back = findViewById(R.id.restart_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+}
